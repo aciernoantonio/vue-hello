89bd10cf7b593c2b317da8f889f1bdd68431733f
aciernoantonio <aciernoantonio.int@gmail.com>
2022-04-20 14:58:24 +0200

    add img print with data

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = new Vue ({
 
     data: {
         //stampare messaggio in h1 attraverso data
-        message: `Mi manca viaggio a Londra`
+        message: `Mi manca viaggio a Londra`,
+
+        //stampare immagine attraverso data
+        imgLink: `https://media.istockphoto.com/photos/london-symbols-with-big-ben-double-decker-buses-and-red-phone-booth-picture-id1294454411?b=1&k=20&m=1294454411&s=170667a&w=0&h=UOVuoyqF8H9J3Q5u-HxrWQFRQVQnGgcoF_L8SVbhxQQ=`
     }
 })
 
